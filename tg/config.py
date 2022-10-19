1b7849e03048e570b8e3e57dce9b57f94a433f5a
Maxim Mosin <max@mosin.pw>
2022-10-20 08:59:03 +0900

    Start making TG base Create dockerfile, reqs, config and update docker-compose

---
+++ tg/config.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+#
+#  MoneyTracker - Settings
+#  Created by LulzLoL231 at 20/10/22
+#
+import logging
+import platform
+
+from pydantic import BaseSettings, Field, PostgresDsn
+
+
+# Fixing .env file path for Docker.
+if platform.system() == 'Linux':
+    env_file = '/tg/.env'
+else:
+    env_file = '.env'
+
+
+class Config(BaseSettings):
+    VERSION: str = '0.1'
+    DEBUG: bool = Field(False, env='BOT_DEBUG')
+    token: str = Field(..., env='BOT_TOKEN')
+    postgres_dsn: PostgresDsn = Field(..., env='BOT_POSTGRESDSN')
+
+    class Config:
+        env_file = env_file
+
+
+cfg = Config()  # type: ignore
+logging.basicConfig(
+    format='[%(levelname)s] %(name)s (%(lineno)d) '
+           '>> %(module)s.%(funcName)s: %(message)s',
+    level=logging.DEBUG if cfg.DEBUG else logging.INFO
+)
